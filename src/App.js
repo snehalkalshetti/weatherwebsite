655252aa7ba33c06d6f951d5439e5df46b999c75
Durgesh Kudalkar <dkudalkarsr2j@gmail.com>
2021-10-23 23:37:05 +0530

    minor changes before backend

--- src/App.js
+++ src/App.js
@@ -4,30 +4,27 @@ import InfoPanel from './info';
 
 function App() {
   return (
-    <>
-   <div class="container" >
-       
-       <div class="containerL">
-        <InfoPanel/>
-       </div>
-    
-       <div class="containerR">
-       <h1 class="text">weather.com</h1>
-       {/* <div class="icon"> */}
-         <img src={img1} class="icon"/>
-          {/* </div> */}
-       <h1 class="degree">29.8&#176;</h1>
-        <div class="info">
-            <h3 class="city">London</h3> 
-            <h6 class="date">Sunday 21,Sept 2021</h6>
+
+    <div className="container" >
+      <div className="containerL">
+        <InfoPanel />
+      </div>
+
+      <div className="containerR">
+        <h1 className="text">weather.com</h1>
+        <div className='weather-card'>
+          <img src={img1} className="icon" />
+          <h1 className="degree">29.8&#176;</h1>
+          <div className="info">
+            <h3 className="city">London</h3>
+            <h6 className="date">Sunday 21, Sept 2021</h6>
+          </div>
         </div>
-       </div>
+      </div>
 
-   </div>
-    
+    </div>
 
-    </>
-    
+
   );
 }
 
